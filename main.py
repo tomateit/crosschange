780de35d70913f1ad2fc879184e8ccfeba2cbbf3
Maksim Ermakov <maksim_ermakov@protonmail.com>
2022-04-26 17:25:18 +0300

    Changed flow after field tests :)

--- main.py
+++ main.py
@@ -22,17 +22,17 @@ def send_welcome(message):
     bot.reply_to(message, "I will help :). Enter a ticker for your local currency:")
 
 
-@bot.message_handler(commands=['enter'])
-def enter(message):
-    user = memory.get(message.from_user.id)
-    if not user:
-        logger.warning(f"No user with id {message.from_user} in memory")
-        bot.reply_to(message, "Sowwy, a vewy bad ewwor occuwed, pwease restawt the bot completely <3")
-        return
+# @bot.message_handler(commands=['enter'])
+# def enter(message):
+#     user = memory.get(message.from_user.id)
+#     if not user:
+#         logger.warning(f"No user with id {message.from_user} in memory")
+#         bot.reply_to(message, "Sowwy, a vewy bad ewwor occuwed, pwease restawt the bot completely <3")
+#         return
 
-    if user.stage != Stage.GET_NEW_TICKER:
-        user.stage = Stage.GET_NEW_TICKER
-        bot.reply_to(message, "Ok, whatever, enter a ticker")
+#     if user.stage != Stage.GET_NEW_TICKER:
+#         user.stage = Stage.GET_NEW_TICKER
+#         bot.reply_to(message, "Ok, whatever, enter a ticker")
 
 
 @bot.message_handler(commands=['me'])
@@ -57,22 +57,21 @@ def exchange(message):
         cur0 = user.current_currency.ticker
         cur1, cur2, *_ = list(user.currencies.values())
         response = f"""
-        {cur1.ticker} <-> {cur0}
-        For 1 {cur0} you can get {1/cur1.buy} {cur1.ticker}
-        For 1 {cur0} you can buy {1/cur1.sell} {cur1.ticker}
-        For 1 {cur1.ticker} you can get {cur1.sell} {cur0}
+{cur1.ticker} <-> {cur0}
+For 1 {cur0} you can buy {(1/cur1.sell):.2f} {cur1.ticker}
+To get 1 {cur0} you need {(1/cur1.buy):.2f} {cur1.ticker}
+For 1 {cur1.ticker} you can get {cur1.sell} {cur0}
 
-        {cur2.ticker} <-> {cur0}
-        For 1 {cur0} you can get {1/cur2.buy} {cur2.ticker}
-        For 1 {cur0} you can buy {1/cur2.sell} {cur2.ticker}
-        For 1 {cur2.ticker} you can get {cur2.sell} {cur0}
+{cur2.ticker} <-> {cur0}
+For 1 {cur0} you can buy {(1/cur2.sell):.2f} {cur2.ticker}
+To get 1 {cur0} you need {(1/cur2.buy):.2f} {cur2.ticker}
+For 1 {cur2.ticker} you can get {cur2.sell} {cur0}
 
-
-        {cur1.ticker} <-> {cur2.ticker}
-        For 1 {cur1.ticker} you can buy {cur1.sell/cur2.sell} {cur2.ticker}
-        For 1 {cur1.ticker} you can get {cur1.buy/cur2.buy} {cur2.ticker}  
-        For 1 {cur2.ticker} you can buy {cur2.sell/cur1.sell} {cur1.ticker}
-        For 1 {cur2.ticker} you can get {cur2.buy/cur1.buy} {cur1.ticker}
+{cur1.ticker} <-> {cur2.ticker}
+For 1 {cur1.ticker} you can buy {(cur1.sell/cur2.sell):2f} {cur2.ticker}
+To get 1 {cur1.ticker} you need {(cur1.buy/cur2.buy):.2f} {cur2.ticker}  
+For 1 {cur2.ticker} you can buy {(cur2.sell/cur1.sell):.2f} {cur1.ticker}
+To get 1 {cur2.ticker} you need {(cur2.buy/cur1.buy):.2f} {cur1.ticker}
         """
         bot.reply_to(message, response)
 
@@ -93,13 +92,18 @@ def main(message):
         if re.match("^\w\w\w$", message.text.strip()):
             user.current_currency = Currency(ticker=message.text.strip().upper())
             user.stage = Stage.GET_NEW_TICKER
-            bot.reply_to(message, "Thanks! Now I need a foreign currency. Enter a ticker:")
+            bot.reply_to(message, "Thanks! Now I need a foreign currency. Enter a foreign currency like 'USD' or 'EUR 3.09 3.15' :")
         else:
             bot.reply_to(message, "Sorry, your response is not what I expect. Please, type a ticker, like USD or GBP")
 
     elif user.stage == Stage.GET_NEW_TICKER:
         ticker = message.text.strip().upper()
-        if re.match("^\w\w\w$", ticker):
+        if re.match("^\w\w\w\s+\d+\.\d+\s+\d+\.\d+$", ticker):
+            ticker, buy, sell = ticker.split(" ")
+            buy, sell = float(buy), float(sell)
+            user.currencies[ticker] = Currency(ticker=ticker, buy=buy, sell=sell)
+            bot.reply_to(message, f"Got ya. {ticker} was updated")
+        elif re.match("^\w\w\w$", ticker):
             user.currencies[ticker] = Currency(ticker=ticker)
             user.selected = user.currencies[ticker]
             user.stage = Stage.GET_VALUE
@@ -112,13 +116,13 @@ def main(message):
             buy, sell = [float(x) for x in value.split(" ")]
             user.currencies[user.selected.ticker].buy = buy
             user.currencies[user.selected.ticker].sell = sell
-            user.stage = Stage.WAIT_FOR_COMMAND
-            bot.reply_to(message, f"Thank you. you got {user}")
+            user.stage = Stage.GET_NEW_TICKER
+            bot.reply_to(message, f"Thank you. You got {user}")
         except:
             bot.reply_to(message, "Sorry, your response is not what I expect. Please, type buy and sell values like 12.36 12.56")
-    elif user.stage == Stage.WAIT_FOR_COMMAND:
-        command = message.text.strip()
-        logger.debug("Command: " + command)
+    # elif user.stage == Stage.WAIT_FOR_COMMAND:
+    #     command = message.text.strip()
+    #     logger.debug("Command: " + command)
     
 
 bot.infinity_polling() 
